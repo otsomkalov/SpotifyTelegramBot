cc021fdc294314e7012fe0af2ecea8b2ec84ca3a
Oleg Tsomkalov <16070015+otsomkalov@users.noreply.github.com>
2024-02-04 20:09:00 +0200

    Use isolated Azure Functions (#48)

--- src/Bot/Startup.fs
+++ src/Bot/Startup.fs
@@ -1,4 +1,4 @@
-﻿namespace Bot
+﻿module Bot.Startup
 
 open System
 open System.Reflection
@@ -7,84 +7,99 @@ open Bot.Helpers
 open Bot.Services
 open Bot.Services.Spotify
 open Bot.Services.Telegram
-open Microsoft.Azure.Functions.Extensions.DependencyInjection
 open Microsoft.EntityFrameworkCore
 open Microsoft.Extensions.DependencyInjection
 open Microsoft.Extensions.Configuration
+open Microsoft.Extensions.Hosting
+open Microsoft.Extensions.Logging
+open Microsoft.Extensions.Logging.ApplicationInsights
 open Microsoft.Extensions.Options
 open SpotifyAPI.Web
 open Telegram.Bot
+open Microsoft.Azure.Functions.Worker
 
 #nowarn "20"
 
-type Startup() =
-  inherit FunctionsStartup()
+let configureTelegram (provider: IServiceProvider) =
+  let settings =
+    provider
+      .GetRequiredService<IOptions<Settings.Telegram.T>>()
+      .Value
 
-  let configureTelegram (provider: IServiceProvider) =
-    let settings =
-      provider
-        .GetRequiredService<IOptions<Settings.Telegram.T>>()
-        .Value
+  TelegramBotClient(settings.Token) :> ITelegramBotClient
 
-    TelegramBotClient(settings.Token) :> ITelegramBotClient
+let configureSpotify (provider: IServiceProvider) =
+  let settings =
+    provider
+      .GetRequiredService<IOptions<Settings.Spotify.T>>()
+      .Value
 
-  let configureSpotify (provider: IServiceProvider) =
-    let settings =
-      provider
-        .GetRequiredService<IOptions<Settings.Spotify.T>>()
-        .Value
+  let config =
+    SpotifyClientConfig
+      .CreateDefault()
+      .WithAuthenticator(ClientCredentialsAuthenticator(settings.ClientId, settings.ClientSecret))
 
-    let config =
-      SpotifyClientConfig
-        .CreateDefault()
-        .WithAuthenticator(ClientCredentialsAuthenticator(settings.ClientId, settings.ClientSecret))
+  SpotifyClient(config) :> ISpotifyClient
 
-    SpotifyClient(config) :> ISpotifyClient
+let configureDbContext (provider: IServiceProvider) (builder: DbContextOptionsBuilder) =
+  let settings =
+    provider
+      .GetRequiredService<IOptions<Settings.Database.T>>()
+      .Value
 
-  let configureDbContext (provider: IServiceProvider) (builder: DbContextOptionsBuilder) =
-    let settings =
-      provider
-        .GetRequiredService<IOptions<Settings.Database.T>>()
-        .Value
+  builder.UseNpgsql(settings.ConnectionString)
 
-    builder.UseNpgsql(settings.ConnectionString)
+  builder.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking)
 
-    builder.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking)
+  ()
 
-    ()
+let configureServices (builderContext: HostBuilderContext) (services: IServiceCollection) : unit =
+  let configuration = builderContext.Configuration
 
-  override this.Configure(builder: IFunctionsHostBuilder) : unit =
-    let configuration =
-      builder.GetContext().Configuration
-    let services = builder.Services
+  services.AddApplicationInsightsTelemetryWorkerService()
+  services.ConfigureFunctionsApplicationInsights();
 
-    (services, configuration)
-    |> Startup.ConfigureAndValidate<Settings.Telegram.T> Settings.Telegram.SectionName
-    |> Startup.ConfigureAndValidate<Settings.Spotify.T> Settings.Spotify.SectionName
-    |> Startup.ConfigureAndValidate<Settings.Database.T> Settings.Database.SectionName
+  (services, configuration)
+  |> Startup.ConfigureAndValidate<Settings.Telegram.T> Settings.Telegram.SectionName
+  |> Startup.ConfigureAndValidate<Settings.Spotify.T> Settings.Spotify.SectionName
+  |> Startup.ConfigureAndValidate<Settings.Database.T> Settings.Database.SectionName
 
-    services.AddDbContext<AppDbContext>(configureDbContext)
+  services.AddDbContext<AppDbContext>(configureDbContext)
 
-    services
-      .AddSingleton<ITelegramBotClient>(configureTelegram)
-      .AddSingleton<ISpotifyClient>(configureSpotify)
+  services
+    .AddSingleton<ITelegramBotClient>(configureTelegram)
+    .AddSingleton<ISpotifyClient>(configureSpotify)
 
-    services
-      .AddSingleton<SpotifyRefreshTokenStore>()
-      .AddSingleton<SpotifyClientStore>()
+  services
+    .AddSingleton<SpotifyRefreshTokenStore>()
+    .AddSingleton<SpotifyClientStore>()
 
-      .AddScoped<SpotifyClientProvider>()
-      .AddScoped<SpotifyService>()
-      .AddScoped<MessageService>()
-      .AddScoped<InlineQueryService>()
+    .AddScoped<SpotifyClientProvider>()
+    .AddScoped<SpotifyService>()
+    .AddScoped<MessageService>()
+    .AddScoped<InlineQueryService>()
 
-    ()
+  services.AddMvcCore().AddNewtonsoftJson()
 
-  override this.ConfigureAppConfiguration(builder: IFunctionsConfigurationBuilder) =
+  ()
 
-    builder.ConfigurationBuilder.AddUserSecrets(Assembly.GetExecutingAssembly(), true)
+let private configureLogging (builder: ILoggingBuilder) =
+  builder.AddFilter<ApplicationInsightsLoggerProvider>(String.Empty, LogLevel.Information)
 
-    ()
+  ()
 
-[<assembly: FunctionsStartup(typeof<Startup>)>]
-do ()
+let configureAppConfiguration _ (configBuilder: IConfigurationBuilder) =
+
+  configBuilder.AddUserSecrets(Assembly.GetExecutingAssembly(), true)
+
+  ()
+
+let host =
+  HostBuilder()
+    .ConfigureFunctionsWebApplication()
+    .ConfigureAppConfiguration(configureAppConfiguration)
+    .ConfigureLogging(configureLogging)
+    .ConfigureServices(configureServices)
+    .Build()
+
+host.Run()
